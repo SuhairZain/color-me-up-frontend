eb6d6d8cf418d72b83477814503a0d507ea57f1d
Suhair Zain <suhair@languagezen.com>
2020-07-14 19:49:07 +0530

    Updates

---
+++ precache-manifest.1f44544d413a0a437831f74f5bcb41b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59609ec99373a10f89647aadf3f609ba",
+    "url": "/color-me-up-frontend/index.html"
+  },
+  {
+    "revision": "38fc89ca49065d3b7a21",
+    "url": "/color-me-up-frontend/static/css/main.0699eb5d.chunk.css"
+  },
+  {
+    "revision": "568fa145c15c709e6e41",
+    "url": "/color-me-up-frontend/static/js/2.978d27e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-me-up-frontend/static/js/2.978d27e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38fc89ca49065d3b7a21",
+    "url": "/color-me-up-frontend/static/js/main.0697f747.chunk.js"
+  },
+  {
+    "revision": "b4073d0fa6b68122ddd4",
+    "url": "/color-me-up-frontend/static/js/runtime-main.8e93a261.js"
+  }
+]);
